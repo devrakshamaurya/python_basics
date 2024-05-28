c833ba10c418ffa65beb3519d76319322af00573
Raksha Maurya <rakshamaurya278@gmail.com>
2024-05-28 14:49:36 +0530

    numbers in depth

---
+++ numbersindepth.py
@@ -0,0 +1,31 @@
+x = 2
+y = 3
+z = 4
+print(x+y)
+
+#random choice
+
+import random
+l1 = ['lemon', 'ginger', 'mint', 'masala']
+random.choice(l1)
+print(random.choice(l1))
+print(random.choice(l1))
+print(random.choice(l1))
+print(random.choice(l1))
+
+
+#set
+
+set = {1, 2, 3, 4}
+print(set&{1, 3})
+print(set|{1,3})
+print(set-{1, 2, 3, 4})
+print(type ({}))
+
+
+print(type(True))
+x =True + 4
+print(x)
+
+
+
