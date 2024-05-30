8db2e1dc08848aec4dbccc34f5d658687fede8a7
Raksha Maurya <rakshamaurya278@gmail.com>
2024-05-30 15:06:48 +0530

    problem 1

---
+++ Conditional_problems/problem1.py
@@ -0,0 +1,13 @@
+#Ques: Age Group Categorization?
+#( Classify a person's age group: Child(<13), Teenager (13-19), Adult(20-59), Senior(60+))
+
+age = 25
+
+if age < 13:
+    print("Child")
+elif age < 20:
+    print("Teenager")
+elif age < 60:
+     print("Adult")
+else:
+     print("Senior")         
