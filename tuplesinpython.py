14f649ff9c627a5db4ef47dfcbe8ad40f6d65415
Raksha Maurya <rakshamaurya278@gmail.com>
2024-05-30 14:07:41 +0530

    tuples

---
+++ tuplesinpython.py
@@ -0,0 +1,10 @@
+tea_types = ("Black", "Green", "Oolong")
+print(tea_types)
+print(len(tea_types))
+more_tea = ("Herbal", "Earl Grey")
+all_tea = more_tea + tea_types
+print(all_tea)
+
+if "Green" in all_tea:
+    print("I have green tea")
+
